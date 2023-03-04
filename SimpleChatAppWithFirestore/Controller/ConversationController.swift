338602e331bb32239de856ef65dd15daba220b56
Tazo <tazogigitashvili@gmail.com>
2023-03-04 13:38:30 +0400

    Created nav title and leftbarbutton with item showprofile

--- SimpleChatAppWithFirestore/Controller/ConversationController.swift
+++ SimpleChatAppWithFirestore/Controller/ConversationController.swift
@@ -15,8 +15,21 @@ class ConversationController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .red
+        configureUI()
     }
     
+    @objc func showProfile() {
+        print(123)
+    }
     
+    func configureUI() {
+        view.backgroundColor = .white
+        
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.title = "Messages"
+        
+        let image = UIImage(systemName: "person.circle.fill")
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: image, style: .plain, target: self, action: #selector(showProfile))
+        
+    }
 }
