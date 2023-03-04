e3d2fa0fe46f3287cb9a9600c304864a15eace16
Tazo <tazogigitashvili@gmail.com>
2023-03-04 14:30:27 +0400

    started configuring tableView

--- SimpleChatAppWithFirestore/Controller/ConversationController.swift
+++ SimpleChatAppWithFirestore/Controller/ConversationController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class ConversationController: UIViewController {
     
-    
+    private let tableView = UITableView()
     
     
     override func viewDidLoad() {
@@ -25,11 +25,36 @@ class ConversationController: UIViewController {
     func configureUI() {
         view.backgroundColor = .white
         
-        navigationController?.navigationBar.prefersLargeTitles = true
-        navigationItem.title = "Messages"
+        configureNavigationBar()
+        configureTableView()
         
         let image = UIImage(systemName: "person.circle.fill")
         navigationItem.leftBarButtonItem = UIBarButtonItem(image: image, style: .plain, target: self, action: #selector(showProfile))
+    }
+    
+    func configureTableView() {
+        tableView.backgroundColor = .systemPink
+        
+        view.addSubview(tableView)
+        tableView.frame = view.frame
+    }
+    
+    func configureNavigationBar() {
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+        appearance.backgroundColor = .systemPurple
+        
+        navigationController?.navigationBar.standardAppearance = appearance
+        navigationController?.navigationBar.compactAppearance = appearance
+        navigationController?.navigationBar.scrollEdgeAppearance = appearance
+        
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.title = "Messages"
+        navigationController?.navigationBar.tintColor = .white
+        navigationController?.navigationBar.isTranslucent = true
+        
+        navigationController?.navigationBar.overrideUserInterfaceStyle = .dark
         
     }
 }
