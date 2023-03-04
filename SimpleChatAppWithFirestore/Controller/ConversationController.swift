eb874cad1bf7429ee86543c3a7b4239e2f91a256
Tazo <tazogigitashvili@gmail.com>
2023-03-04 13:26:55 +0400

    created navigation controller and root as conversation controller

---
+++ SimpleChatAppWithFirestore/Controller/ConversationController.swift
@@ -0,0 +1,22 @@
+//
+//  ConversationController.swift
+//  SimpleChatAppWithFirestore
+//
+//  Created by Tazo Gigitashvili on 04.03.23.
+//
+
+import UIKit
+
+class ConversationController: UIViewController {
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+    
+    
+}
