69234da0d82d5055733f348f31654e6bb080843f
Tazo <tazogigitashvili@gmail.com>
2023-03-11 12:41:44 +0400

    Created and modified UserCell UI, profile image and 2 labels in stack. added this cell in NewMessageController

--- SimpleChatAppWithFirestore/NewMessageController.swift
+++ SimpleChatAppWithFirestore/NewMessageController.swift
@@ -35,7 +35,7 @@ class NewMessageController: UITableViewController {
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(handleDissmisal))
         
         tableView.tableFooterView = UIView()
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: reuseIdentifier)
+        tableView.register(UserCell.self, forCellReuseIdentifier: reuseIdentifier)
         tableView.rowHeight = 80
     }
 }
@@ -48,8 +48,7 @@ extension NewMessageController {
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
-        cell.textLabel?.text = "Test Cell"
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! UserCell
         return cell
     }
     
